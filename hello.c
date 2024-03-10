05c91df21654467497f81fb2202023d4ceec0e29
kewei <huangkewei1998@gmail.com>
2024-03-10 13:38:27 +0000

    initial project

---
+++ hello.c
@@ -0,0 +1,15 @@
+/* 
+ * Name: Kewei
+ * 
+ */
+// Hello
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a = 10;
+    printf("a = %d\n", a);
+    printf("hello world!\n");
+    return 0;
+}
